a998170b53b365054f15417f0916ca232d3063d8
n0e <noemy.garcia@gmail.com>
2022-03-14 10:43:45 +0100

    [ADD] Script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let btnSubmit = document.querySelector('input[type="submit"]');
+let aNamePass = [];
+
+btnSubmit.addEventListener('submit', function (event) {
+    let name = document.getElementById('name');
+    let pass = document.getElementById('pass');
+
+    let oAux = {
+        user: name,
+        pass: pass
+    };
+
+    aNamePass.push(oAux);
+
+    console.log(aNamePass);
+});
